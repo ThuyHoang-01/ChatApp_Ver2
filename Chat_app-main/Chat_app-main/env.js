004024ef5904d6de08e18f6e159533ec2c15361a
Hoàng Thị Tố Thùy <httthuy455@gmail.com>
2022-11-28 12:06:20 +0700

    update project ver2
    update project ver2

---
+++ Chat_app-main/Chat_app-main/env.js
@@ -0,0 +1,21 @@
+export const fbConfig = {
+  // apiKey: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+  // authDomain: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+  // projectId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+  // storageBucket: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+  // messagingSenderId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx,
+  // appId: xxx-xxx-xxx-xxx-xxx-xxx-xxx-xxx
+  apiKey: "AIzaSyDv8Sa3nEhHa94XnyUhbZvnUR74npS5HZE",
+  authDomain: "new-chat-app-b839a.firebaseapp.com",
+  projectId: "new-chat-app-b839a",
+  storageBucket: "new-chat-app-b839a.appspot.com",
+  messagingSenderId: "595102254639",
+  appId: "1:595102254639:web:5c924f914bf228547de8da",
+};
+
+export const cometChatConfig = {
+  cometChatAppId: "2244717be748afd4",
+  cometChatRegion: "us",
+  cometChatAuthKey: "532cb5542fa119d3191c748000eaca2a5da093ad",
+  cometChatRestApiKey: "971f0e95c602cbf2b7d02b039b48da2ce041a746",
+};
